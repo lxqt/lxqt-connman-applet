fefd0c9e25a9aa3ac0c56a01ec90c4bfb4e25426
Christian Surlykke <christian@surlykke.dk>
2015-08-02 14:03:48 +0200

    Add ts template

---
+++ translations/lxqt-connman-applet.ts
@@ -0,0 +1,80 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.1">
+<context>
+    <name>Dialog</name>
+    <message>
+        <location filename="../dialog.ui" line="14"/>
+        <source>Dialog</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../dialog.ui" line="25"/>
+        <source>Connect to %1</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>QObject</name>
+    <message>
+        <location filename="../ui_strings.cpp" line="36"/>
+        <source>name</source>
+        <comment>network name</comment>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../ui_strings.cpp" line="37"/>
+        <location filename="../ui_strings.cpp" line="38"/>
+        <source>Username</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../ui_strings.cpp" line="39"/>
+        <source>Passphrase</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../ui_strings.cpp" line="40"/>
+        <source>Previous passphrase</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../ui_strings.cpp" line="42"/>
+        <source>Wired</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../ui_strings.cpp" line="43"/>
+        <source>Wireless</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../ui_strings.cpp" line="44"/>
+        <source>Peer-to-peer</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../ui_strings.cpp" line="45"/>
+        <source>Bluetooth</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>SystemTray</name>
+    <message>
+        <location filename="../systemtray.cpp" line="43"/>
+        <source>Quit</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../systemtray.cpp" line="78"/>
+        <source>Technologies:</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../systemtray.cpp" line="88"/>
+        <source>Services: </source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+</TS>
