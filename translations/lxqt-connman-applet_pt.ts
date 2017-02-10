090790709aa8035b4bc1e15b40f27f264c03b010
fadoaglauss <fadoa.glauss@gmail.com>
2017-02-10 13:17:15 -0300

    Creat lxqt-connman-applet_pt.ts
    Portuguese translation

---
+++ translations/lxqt-connman-applet_pt.ts
@@ -0,0 +1,80 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.1" language="pt-br">
+<context>
+    <name>Dialog</name>
+    <message>
+        <location filename="../dialog.ui" line="14"/>
+        <source>LXQt ConnMan applet</source>
+        <translation>LXQt ConnMan applet</translation>
+    </message>
+    <message>
+        <location filename="../dialog.ui" line="25"/>
+        <source>Connect to %1</source>
+        <translation>Conectando em %1</translation>
+    </message>
+</context>
+<context>
+    <name>QObject</name>
+    <message>
+        <location filename="../ui_strings.cpp" line="36"/>
+        <source>name</source>
+        <comment>network name</comment>
+        <translation>Nome da rede</translation>
+    </message>
+    <message>
+        <location filename="../ui_strings.cpp" line="37"/>
+        <location filename="../ui_strings.cpp" line="38"/>
+        <source>Username</source>
+        <translation>Nome de Usuário</translation>
+    </message>
+    <message>
+        <location filename="../ui_strings.cpp" line="39"/>
+        <source>Passphrase</source>
+        <translation>Senha</translation>
+    </message>
+    <message>
+        <location filename="../ui_strings.cpp" line="40"/>
+        <source>Previous passphrase</source>
+        <translation>Última senha</translation>
+    </message>
+    <message>
+        <location filename="../ui_strings.cpp" line="42"/>
+        <source>Wired</source>
+        <translation>Com fio</translation>
+    </message>
+    <message>
+        <location filename="../ui_strings.cpp" line="43"/>
+        <source>Wireless</source>
+        <translation>Wi-fi</translation>
+    </message>
+    <message>
+        <location filename="../ui_strings.cpp" line="44"/>
+        <source>Peer-to-peer</source>
+        <translation>ponto-a-ponto (P2P)</translation>
+    </message>
+    <message>
+        <location filename="../ui_strings.cpp" line="45"/>
+        <source>Bluetooth</source>
+        <translation>Bluetooth</translation>
+    </message>
+</context>
+<context>
+    <name>SystemTray</name>
+    <message>
+        <location filename="../systemtray.cpp" line="43"/>
+        <source>Quit</source>
+        <translation>Sair</translation>
+    </message>
+    <message>
+        <location filename="../systemtray.cpp" line="78"/>
+        <source>Technologies:</source>
+        <translation>Tecnologias:</translation>
+    </message>
+    <message>
+        <location filename="../systemtray.cpp" line="88"/>
+        <source>Services: </source>
+        <translation>Serviços:</translation>
+    </message>
+</context>
+</TS>
