2afc382c5e627782fd71431f871d67fc1a4d6d65
Marcin Mikołajczak <me@m4sk.in>
2017-05-22 18:26:47 +0200

    updated Polish translation
    Signed-off-by: Marcin Mikołajczak <me@m4sk.in>

---
+++ translations/lxqt-connman-applet_pl.ts
@@ -0,0 +1,80 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.1">
+<context>
+    <name>Dialog</name>
+    <message>
+        <location filename="../dialog.ui" line="14"/>
+        <source>LXQt ConnMan applet</source>
+        <translation>Aplet ConnMan dla LXQt</translation>
+    </message>
+    <message>
+        <location filename="../dialog.ui" line="25"/>
+        <source>Connect to %1</source>
+        <translation>Połącz z %1</translation>
+    </message>
+</context>
+<context>
+    <name>QObject</name>
+    <message>
+        <location filename="../ui_strings.cpp" line="36"/>
+        <source>name</source>
+        <comment>network name</comment>
+        <translation>nazwa</translation>
+    </message>
+    <message>
+        <location filename="../ui_strings.cpp" line="37"/>
+        <location filename="../ui_strings.cpp" line="38"/>
+        <source>Username</source>
+        <translation>Nazwa użytkownika</translation>
+    </message>
+    <message>
+        <location filename="../ui_strings.cpp" line="39"/>
+        <source>Passphrase</source>
+        <translation>Hasło</translation>
+    </message>
+    <message>
+        <location filename="../ui_strings.cpp" line="40"/>
+        <source>Previous passphrase</source>
+        <translation>Poprzednie hasło</translation>
+    </message>
+    <message>
+        <location filename="../ui_strings.cpp" line="42"/>
+        <source>Wired</source>
+        <translation>Kablowe</translation>
+    </message>
+    <message>
+        <location filename="../ui_strings.cpp" line="43"/>
+        <source>Bezprzewodowe</source>
+        <translation></translation>
+    </message>
+    <message>
+        <location filename="../ui_strings.cpp" line="44"/>
+        <source>Peer-to-peer</source>
+        <translation>P2P</translation>
+    </message>
+    <message>
+        <location filename="../ui_strings.cpp" line="45"/>
+        <source>Bluetooth</source>
+        <translation>Bluetooth</translation>
+    </message>
+</context>
+<context>
+    <name>SystemTray</name>
+    <message>
+        <location filename="../systemtray.cpp" line="43"/>
+        <source>Quit</source>
+        <translation>Wyjdź</translation>
+    </message>
+    <message>
+        <location filename="../systemtray.cpp" line="78"/>
+        <source>Technologies:</source>
+        <translation>Technologie:</translation>
+    </message>
+    <message>
+        <location filename="../systemtray.cpp" line="88"/>
+        <source>Services: </source>
+        <translation>Serwisy:</translation>
+    </message>
+</context>
+</TS>
