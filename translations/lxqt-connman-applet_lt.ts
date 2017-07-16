69671087658b78cf30f5375033e5818432462170
welaq <welaq@users.noreply.github.com>
2017-07-17 02:45:33 +0300

    Added Lithuanian translation

---
+++ translations/lxqt-connman-applet_lt.ts
@@ -0,0 +1,80 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.1" language="lt">
+<context>
+    <name>Dialog</name>
+    <message>
+        <location filename="../dialog.ui" line="14"/>
+        <source>LXQt ConnMan applet</source>
+        <translation>LXQt ConnMan programėlė</translation>
+    </message>
+    <message>
+        <location filename="../dialog.ui" line="25"/>
+        <source>Connect to %1</source>
+        <translation>Prisijungti prie %1</translation>
+    </message>
+</context>
+<context>
+    <name>QObject</name>
+    <message>
+        <location filename="../ui_strings.cpp" line="36"/>
+        <source>name</source>
+        <comment>network name</comment>
+        <translation>pavadinimas</translation>
+    </message>
+    <message>
+        <location filename="../ui_strings.cpp" line="37"/>
+        <location filename="../ui_strings.cpp" line="38"/>
+        <source>Username</source>
+        <translation>Naudotojo vardas</translation>
+    </message>
+    <message>
+        <location filename="../ui_strings.cpp" line="39"/>
+        <source>Passphrase</source>
+        <translation>Slaptafrazė</translation>
+    </message>
+    <message>
+        <location filename="../ui_strings.cpp" line="40"/>
+        <source>Previous passphrase</source>
+        <translation>Ankstesnė slaptafrazė</translation>
+    </message>
+    <message>
+        <location filename="../ui_strings.cpp" line="42"/>
+        <source>Wired</source>
+        <translation>Laidinis</translation>
+    </message>
+    <message>
+        <location filename="../ui_strings.cpp" line="43"/>
+        <source>Wireless</source>
+        <translation>Belaidis</translation>
+    </message>
+    <message>
+        <location filename="../ui_strings.cpp" line="44"/>
+        <source>Peer-to-peer</source>
+        <translation>P2P</translation>
+    </message>
+    <message>
+        <location filename="../ui_strings.cpp" line="45"/>
+        <source>Bluetooth</source>
+        <translation>Bluetooth</translation>
+    </message>
+</context>
+<context>
+    <name>SystemTray</name>
+    <message>
+        <location filename="../systemtray.cpp" line="43"/>
+        <source>Quit</source>
+        <translation>Išeiti</translation>
+    </message>
+    <message>
+        <location filename="../systemtray.cpp" line="78"/>
+        <source>Technologies:</source>
+        <translation>Technologijos:</translation>
+    </message>
+    <message>
+        <location filename="../systemtray.cpp" line="88"/>
+        <source>Services: </source>
+        <translation>Tarnybos: </translation>
+    </message>
+</context>
+</TS>
