f20ba88ea53e576f9d88970c6414c240a6b668e5
Alf Gaida <agaida@siduction.org>
2019-09-22 18:32:01 +0200

    Translation source update

--- translations/lxqt-connman-applet.ts
+++ translations/lxqt-connman-applet.ts
@@ -2,78 +2,183 @@
 <!DOCTYPE TS>
 <TS version="2.1">
 <context>
+    <name>Controller</name>
+    <message>
+        <location filename="../controller.cpp" line="58"/>
+        <source>Services...</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../controller.cpp" line="59"/>
+        <location filename="../controller.cpp" line="156"/>
+        <source>About</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../controller.cpp" line="60"/>
+        <source>Quit</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../controller.cpp" line="157"/>
+        <source>&lt;p&gt;  &lt;b&gt;LXQt Connman Client&lt;/b&gt;&lt;/p&gt;&lt;p&gt;Copyright 2014, 2015, 2016&lt;/p&gt;&lt;p&gt;Christian Surlykke&lt;/p&gt;</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
     <name>Dialog</name>
     <message>
-        <location filename="../dialog.ui" line="14"/>
+        <location filename="../agentdialog.ui" line="14"/>
         <source>LXQt ConnMan applet</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="../dialog.ui" line="25"/>
+        <location filename="../agentdialog.ui" line="25"/>
         <source>Connect to %1</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>IconViewer</name>
+    <message>
+        <location filename="../iconviewer/iconviewer.ui" line="14"/>
+        <source>Icons</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../iconviewer/iconviewer.ui" line="26"/>
+        <location filename="../iconviewer/iconviewer.ui" line="46"/>
+        <location filename="../iconviewer/iconviewer.ui" line="66"/>
+        <location filename="../iconviewer/iconviewer.ui" line="86"/>
+        <location filename="../iconviewer/iconviewer.ui" line="106"/>
+        <location filename="../iconviewer/iconviewer.ui" line="126"/>
+        <location filename="../iconviewer/iconviewer.ui" line="146"/>
+        <source>I</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../iconviewer/iconviewer.ui" line="33"/>
+        <source>Connected</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../iconviewer/iconviewer.ui" line="53"/>
+        <source>Disconnected</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../iconviewer/iconviewer.ui" line="73"/>
+        <source>None</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../iconviewer/iconviewer.ui" line="93"/>
+        <source>Weak</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../iconviewer/iconviewer.ui" line="113"/>
+        <source>Ok</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../iconviewer/iconviewer.ui" line="133"/>
+        <source>Good</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../iconviewer/iconviewer.ui" line="153"/>
+        <source>Excellent</source>
         <translation type="unfinished"></translation>
     </message>
 </context>
 <context>
     <name>QObject</name>
     <message>
-        <location filename="../ui_strings.cpp" line="36"/>
+        <location filename="../appletstrings.cpp" line="36"/>
         <source>name</source>
         <comment>network name</comment>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="../ui_strings.cpp" line="37"/>
-        <location filename="../ui_strings.cpp" line="38"/>
+        <location filename="../appletstrings.cpp" line="37"/>
+        <location filename="../appletstrings.cpp" line="38"/>
         <source>Username</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="../ui_strings.cpp" line="39"/>
+        <location filename="../appletstrings.cpp" line="39"/>
         <source>Passphrase</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="../ui_strings.cpp" line="40"/>
+        <location filename="../appletstrings.cpp" line="40"/>
         <source>Previous passphrase</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="../ui_strings.cpp" line="42"/>
+        <location filename="../appletstrings.cpp" line="42"/>
         <source>Wired</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="../ui_strings.cpp" line="43"/>
+        <location filename="../appletstrings.cpp" line="43"/>
         <source>Wireless</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="../ui_strings.cpp" line="44"/>
+        <location filename="../appletstrings.cpp" line="44"/>
         <source>Peer-to-peer</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="../ui_strings.cpp" line="45"/>
+        <location filename="../appletstrings.cpp" line="45"/>
         <source>Bluetooth</source>
         <translation type="unfinished"></translation>
     </message>
-</context>
-<context>
-    <name>SystemTray</name>
     <message>
-        <location filename="../systemtray.cpp" line="43"/>
-        <source>Quit</source>
+        <location filename="../appletstrings.cpp" line="47"/>
+        <source>Out of range</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="../systemtray.cpp" line="78"/>
-        <source>Technologies:</source>
+        <location filename="../appletstrings.cpp" line="48"/>
+        <source>Pin code missing</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="../systemtray.cpp" line="88"/>
-        <source>Services: </source>
+        <location filename="../appletstrings.cpp" line="49"/>
+        <source>DHCP failed</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../appletstrings.cpp" line="50"/>
+        <source>Connection failed</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../appletstrings.cpp" line="51"/>
+        <source>Login failed</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../appletstrings.cpp" line="52"/>
+        <source>Authentication failed</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../appletstrings.cpp" line="53"/>
+        <source>Invalid key</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../appletstrings.cpp" line="55"/>
+        <source>Configuration</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../appletstrings.cpp" line="56"/>
+        <source>Association</source>
         <translation type="unfinished"></translation>
     </message>
 </context>
